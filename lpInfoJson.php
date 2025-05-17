fce067011ede32be98259f41f4bb976df1c5881b
Fatimasania <fatimasania871@gmail.com>
2025-05-17 22:25:33 +0100

    Delete files

--- lpInfoJson.php
+++
@@ -1,6 +0,0 @@
-<?php
-session_start();
-echo json_encode([
-    'Name' => isset($_SESSION['Name']) ? $_SESSION['Name'] : '',
-    'Description' => isset($_SESSION['Description']) ? $_SESSION['Description'] : '',
-]);
