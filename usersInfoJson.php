8d7e46870681e952bc319a87984f2c2b75c14dfd
Fatimasania <fatimasania871@gmail.com>
2025-05-22 21:57:23 +0100

    Delete service code from usersInfoJson.php

--- usersInfoJson.php
+++ usersInfoJson.php
@@ -60,18 +60,5 @@ if (!empty($connection)) {
     $tablesToJson['LoyaltyProgramTable'] = $loyaltyProgram;
 }
 
-$consultation = "SELECT * FROM service";
-if (!empty($connection)) {
-    $result = mysqli_query($connection, $consultation);
-    $Service = [];
-    if ($result && $result->num_rows > 0) {
-        while ($row = $result->fetch_assoc()) {
-            $Service[] = $row;
-        }
-    } else {
-        $Service[] = [];
-    }
-    $tablesToJson['ServiceTable'] = $Service;
-}
 
 echo json_encode($tablesToJson);
