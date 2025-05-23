c281ed442f32b9d1bf8a43552b3fa73eb1336cc2
Fatimasania <fatimasania871@gmail.com>
2025-05-23 23:20:32 +0100

    ..

--- sql/Availability_table.sql
+++ sql/Availability_table.sql
@@ -5,7 +5,7 @@ CREATE TABLE Availability (
     SDate VARCHAR(255),
     EDate VARCHAR(255),
 	Hours VARCHAR(255),
-	Reson VARCHAR(255),
+	Reason VARCHAR(255),
     IsAvailable boolean,
 	UserID Int,
     FOREIGN KEY (UserID) REFERENCES User(UserID)
